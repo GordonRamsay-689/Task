02bcf563f5c32af8db0bc5ad34cc24851e0c8695
GordonRamsay-689 <172498648+GordonRamsay-689@users.noreply.github.com>
2025-03-24 22:51:17 +0100

    Updated table dict to have a name and contents field

--- init_table.py
+++ init_table.py
@@ -1,7 +1,10 @@
 import json
 
-table = []
+# Local imports
+from globals import *
+
+table = {TBL_KEY_NAME: "", TBL_KEY_CONTENTS: []}
 
 with open("table.json", "w") as table_file:
-    table_file.write(json.dumps(table, ensure_ascii=False))
+    table_file.write(json.dumps(table[TBL_KEY_CONTENTS], ensure_ascii=False))
     
